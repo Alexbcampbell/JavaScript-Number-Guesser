a919b54c9fd0ec79a43b0f67cb4d0b3e187d2c33
alex Campbell <alexbe.campbell@gmail.com>
2022-05-23 23:38:25 -0500

    final commit, added get random number function to pick a random number to guess.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 //Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 //UI elements
@@ -22,6 +22,13 @@ const game = document.querySelector('#game'),
 //Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+//Play again event listener
+game.addEventListener('mousedown', function(e){
+if(e.target.className === 'play-again'){
+    window.location.reload();
+}
+})
 
 //listen for guess
 guessBtn.addEventListener('click', function(){
@@ -83,6 +90,15 @@ function gameOver(won, msg){
     message.style.color = color;
     //set message
     setMessage(msg);
+
+    //Play Again?
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+}
+
+//Get winning number
+function getRandomNum(min, max){
+return(Math.floor(Math.random()*(max-min+1)+min));
 }
 
 //set message
